7b1c27987da93605e9ee5e7789b2ec066faa80a1
Zedude321 <zedude321@gmail.com>
2021-01-04 21:26:35 +0800

    yeah

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebase = require('firebase')
+
+const firebaseConfig = {
+       apiKey: "AIzaSyBQ1yC4ntqvBOKbpmIJjr-8msavCo2OuaE",
+       authDomain: "imdb-collection.firebaseapp.com",
+       projectId: "imdb-collection",
+       storageBucket: "imdb-collection.appspot.com",
+       messagingSenderId: "842483507417",
+       appId: "1:842483507417:web:0bea67c5e6f5e569aa5ec8",
+       measurementId: "G-BRG1M8FF30"
+};
+
+firebase.initalizeApp(firebaseConfig)
--- imdb.js
+++ imdb.js
@@ -1,5 +1,17 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
+const firebase = require('firebase')
+require("firebase/firestore");
+
+const firebaseConfig = {
+       apiKey: "AIzaSyBQ1yC4ntqvBOKbpmIJjr-8msavCo2OuaE",
+       authDomain: "imdb-collection.firebaseapp.com",
+       projectId: "imdb-collection",
+       storageBucket: "imdb-collection.appspot.com",
+       messagingSenderId: "842483507417",
+       appId: "1:842483507417:web:0bea67c5e6f5e569aa5ec8",
+       measurementId: "G-BRG1M8FF30"
+};
 
 const imdbTop100 = 'https://www.imdb.com/search/title/?count=100&groups=top_100&sort=user_rating'
 
@@ -9,14 +21,27 @@ const fetchHTML = async (url) => {
 }
 
 (async () => {
+       await firebase.initializeApp(firebaseConfig);
        const $ = await fetchHTML(imdbTop100)
 
        const movies = $('div.lister-item')
 
-       movies.each((i, element) => {
+       await movies.each((i, element) => {
               const movie = $(element)
               
               console.log('Title: ' + movie.find('.lister-item-header > a').text())
-              console.log('Poster: ' + movie.find('.lister-item-content > p')[4].text())
+              console.log('Date: ' + movie.find('.lister-item-year').text())
+              console.log('Rating: ' + movie.find('.ratings-imdb-rating > strong').text())
+
+              firebase.firestore().collection('100').doc(i + 'number').set({
+                     title: movie.find('.lister-item-header > a').text(),
+                     date: movie.find('.lister-item-year').text(),
+                     rating: movie.find('.ratings-imdb-rating > strong').text(),
+                     rank: i + 1
+              })
        })
+
+       return 0;
 })();
+
+return 0;
